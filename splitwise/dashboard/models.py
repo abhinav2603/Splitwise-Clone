f0784075ba2ab24d59f47dc8585e06f413bd2eab
Saumya Goyal <saumyagoyal01@gmail.com>
2019-11-06 21:15:51 +0530

    Migrate modified models

--- splitwise/dashboard/models.py
+++ splitwise/dashboard/models.py
@@ -13,18 +13,18 @@ class User(models.Model):
 
 class Groups(models.Model):
 	group_id=models.IntegerField(default=0)
-	user_id=models.ForeignKey(User,on_delete=models.CASCADE)
+	user=models.ForeignKey(User,on_delete=models.CASCADE)
 	class Meta:
-		unique_together=(('group_id','user_id'),)
+		unique_together=(('group_id','user'),)
 
 class Transaction(models.Model):
 	trans_type=models.CharField(max_length=10)
 	date=models.DateTimeField()
 
 class TransactionDetail(models.Model):
-	trans_id=models.ForeignKey(Transaction,on_delete=models.CASCADE)
-	user_id=models.ForeignKey(User,on_delete=models.CASCADE)
-	group_id=models.ForeignKey(Groups,on_delete=models.CASCADE)
+	trans=models.ForeignKey(Transaction,on_delete=models.CASCADE)
+	user=models.ForeignKey(User,on_delete=models.CASCADE)
+	group=models.ForeignKey(Groups,on_delete=models.CASCADE)
 	lent=models.FloatField()
 	class Meta:
-		unique_together=(('trans_id','user_id'),)
+		unique_together=(('trans','user'),)
