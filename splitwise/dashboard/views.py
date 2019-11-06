ccbcf7979b8723944b68632d6aa97825b3db6ec0
Saumya Goyal <saumyagoyal01@gmail.com>
2019-11-06 23:22:22 +0530

    Display friends list in dashboard

--- splitwise/dashboard/views.py
+++ splitwise/dashboard/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from .models import User
 
 # Create your views here.
 def index(request):
 	return HttpResponse("Dashboard");
+
+def personal_page(request,user_id):
+	user=get_object_or_404(User, pk=user_id)
+	return render(request,'dashboard/pers_page.html',{'user':user});
