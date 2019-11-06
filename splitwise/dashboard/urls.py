a3c290cc5bc1482accce660a786e59ea92fde330
ABHINAV KUMAR <ad.abhinavkumar.1999@gmail.com>
2019-11-06 23:49:19 +0530

    User Signup

--- splitwise/dashboard/urls.py
+++ splitwise/dashboard/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'dashboard'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('register', views.register, name='register'),
+    #path("logout", views.logout_request, name="logout"),
 ]
--- splitwise/dashboard/views.py
+++ splitwise/dashboard/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import logout, authenticate, login
 
 # Create your views here.
 def index(request):
 	return HttpResponse("Dashboard");
+
+def register(request):
+	if request.method == "POST":
+		form = UserCreationForm(request.POST)
+		user = form.save()
+		username = form.cleaned_data.get('username')
+		login(request,user)
+		return redirect("dashboard:index")
+	
+	form = UserCreationForm
+	return render(request = request,
+			template_name = "dashboard/login.html",
+			context={"form":form})
