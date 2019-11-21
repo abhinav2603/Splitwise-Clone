f05b80a823388d1b5318dd3d445de1c09f44d557
ABHINAV KUMAR <ad.abhinavkumar.1999@gmail.com>
2019-11-21 14:27:51 +0530

    USer-Profile-Pic

--- splitwise/dashboard/models.py
+++ splitwise/dashboard/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User as Duser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class User(models.Model):
@@ -11,6 +14,28 @@ class User(models.Model):
 #	friend2=models.ForeignKey(User,on_delete=models.CASCADE,related_name='User 2')
 #	class Meta:
 #		unique_together=(('friend1','friend2'),)
+
+class Profile(models.Model):
+	user = models.OneToOneField(Duser, on_delete=models.CASCADE)
+	bio = models.TextField(max_length=500, blank=True)
+	image = models.FileField(upload_to='post_image',blank=True)
+	location = models.CharField(max_length=30, blank=True)
+	#birth_date = models.DateField(null=True, blank=True)
+
+#@receiver(post_save, sender=User)
+#def create_user_profile(sender, instance, created, **kwargs):#
+#if created:
+#	Profile.objects.create(user=instance)
+
+#@receiver(post_save, sender=User)
+#def save_user_profile(sender, instance, **kwargs):
+#    instance.profile.save()
+
+#@receiver(post_save, sender=User)
+#def update_user_profile(sender, instance, created, **kwargs):
+#    if created:
+#        Profile.objects.create(user=instance)
+ #   instance.profile.save()
 
 class Group(models.Model):
 	group_name=models.CharField(max_length=20)
--- splitwise/dashboard/views.py
+++ splitwise/dashboard/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import logout, authenticate, login
 from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import User as myUser, Group as myGroup, Transaction, TransactionDetail
+from .forms import ProfileForm
 
 # Create your views here.
 def index(request):
@@ -16,9 +17,16 @@ def index(request):
 def register(request):
 	if request.method == "POST":
 		form = UserCreationForm(request.POST)
+		profile_form = ProfileForm(request.POST)
 		user = form.save()
-		username = form.cleaned_data.get('username')
+		#user.refresh_from_db()  # load the profile instance created by the signal
+		profile = profile_form.save(commit=False)
+		profile.user = user
+
+		user.save()
+		profile.save()
 		login(request,user)
+		username = form.cleaned_data.get('username')
 		messages.success(request, f"New account created: {username}")
 		newUser=myUser(user_name=username)
 		newUser.save()
@@ -27,9 +35,10 @@ def register(request):
 		return redirect("dashboard:index")
 	
 	form = UserCreationForm
+	profile_form = ProfileForm(request.POST)
 	return render(request = request,
 			template_name = "dashboard/register.html",
-			context={"form":form})
+			context={"form":form,"profile_form":profile_form})
 
 def logout_request(request):
 	logout(request)
