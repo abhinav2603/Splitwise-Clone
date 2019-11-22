959523907c678d6314987866dbb0a49c728bbda3
ABHINAV KUMAR <ad.abhinavkumar.1999@gmail.com>
2019-11-22 21:58:52 +0530

    User-Profile

--- splitwise/dashboard/models.py
+++ splitwise/dashboard/models.py
@@ -18,7 +18,7 @@ class User(models.Model):
 class Profile(models.Model):
 	user = models.OneToOneField(Duser, on_delete=models.CASCADE)
 	bio = models.TextField(max_length=500, blank=True)
-	image = models.FileField(upload_to='post_image',blank=True)
+	image = models.ImageField(max_length=100,blank=True)
 	location = models.CharField(max_length=30, blank=True)
 	#birth_date = models.DateField(null=True, blank=True)
 
